10bb9c87ad7321d23eb583cbcffec698706807cc
jybryce4 <jybryce4@gmail.com>
2019-01-16 11:47:12 -0600

    finished problem 1 & changed directory/package structure

---
+++ com/brycejamey/projecteuler/Multiples35.java
@@ -0,0 +1,19 @@
+/* Project Euler Problem 1
+* This program finds the sum of all the multiples of 3 or 5 below 1000
+* Solution by Jamey Bryce
+*/
+package com.brycejamey.projecteuler;
+
+public class Multiples35 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
