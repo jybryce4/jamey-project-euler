b72857336c1007c461a70d3082a314df5001926a
jybryce4 <jybryce4@gmail.com>
2019-01-17 16:26:29 -0600

    had to do other things. going to work tonight. need to be able to download elsewhere so im backing up small changes too

--- java/com/brycejamey/problem2/FibonacciNumber.java
+++ java/com/brycejamey/problem2/FibonacciNumber.java
@@ -5,13 +5,22 @@
  * Author: Jamey Bryce
  */
 
-class FibonacciNumber {
+public class FibonacciNumber {
 
     private int num1;
+    private int num2;
 
     public FibonacciNumber(int num1, int num2) { // two integers representing the two previous numbers
         this.num1 = num1;
         this.num2 = num2;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
     }
 
 }
