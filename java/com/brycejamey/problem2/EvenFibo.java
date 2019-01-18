08d4f25caa5a986f0e059a196e65f61fd0ff5b3f
jybryce4 <jybryce4@gmail.com>
2019-01-17 23:05:11 -0600

    problem 2 solved! deleted unecessary file

--- java/com/brycejamey/problem2/EvenFibo.java
+++ java/com/brycejamey/problem2/EvenFibo.java
@@ -13,31 +13,25 @@ package com.brycejamey.problem2;
 public class EvenFibo {
 
     public static void main(String[] args) {
-        //FibbnoacciNumber fibo = new FibonacciNumber(n1, n2);
 
-        final int FOUR_MIL = 4000000; 
+        final int FOUR_MIL = 4000000; // to prevent hard coding
 
-        int fibNum = 1;
-        int sum = 0;
+        long fibNum1 = 1;
+        long fibNum2 = 1;
+        long evenSum = 0; // answer at the end
 
-        int num1;
-        int num2 = 0;
-
-        for (int i = 0; i < 10; i++) {
-            num1 = ; //having some difficulties will proceed with other idea (FibonacciNumber.java)
-
-            System.out.println("Num1: " + num1); // "debugger"
-
-            num2 = fibNum++ - num1;
-
-            fibNum = num1 + num2;
-            System.out.println("Fibo: " + fibNum); // "debugger"
-
-            if (fibNum % 2 == 0) {
-                sum += fibNum;
-                System.out.println("Sum: " + sum);
+        long currentFibNum = 0;
+        while (currentFibNum < FOUR_MIL) {
+            if ((currentFibNum % 2) == 0) {
+                evenSum += currentFibNum;
             }
+
+            currentFibNum = fibNum1 + fibNum2;
+            fibNum2 = fibNum1;
+            fibNum1 = currentFibNum;
         }
+
+        System.out.println(evenSum);
 
     }
 
