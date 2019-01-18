ff12941d7020bbc1018cf38c649e031e35fb5c04
jybryce4 <jybryce4@gmail.com>
2019-01-18 15:38:20 -0600

    created .java for problem #3

---
+++ java/com/brycejamey/problem3/LargePrimeFactor.java
@@ -0,0 +1,18 @@
+/*
+ * Project Euler Problem 3
+ * @ http://projecteuler.net/problem=3
+ * 
+ * This program finds the largest prime factor of 600851475143
+ * 
+ * Solution by Jamey Bryce
+ */
+
+
+
+package com.brycejamey.problem3;
+
+public class LargePrimeFactor {
+    public static void main(String[] args) {
+
+    }
+}
