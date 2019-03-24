2050c9dd73cda7bf92fb1fc808204ffcd9eba14f
jybryce4 <jybryce4@gmail.com>
2019-03-24 18:52:08 -0500

    got much further along in problem 3

--- java/com/brycejamey/problem3/LargePrimeFactor.java
+++ java/com/brycejamey/problem3/LargePrimeFactor.java
@@ -7,12 +7,51 @@
  * Solution by Jamey Bryce
  */
 
-
-
 package com.brycejamey.problem3;
 
-public class LargePrimeFactor {
-    public static void main(String[] args) {
+import java.math.BigInteger;
 
+public class LargePrimeFactor {
+    /*
+     * A composite number N can be represented as a product of its prime factors
+     * General: N = p1 * p2 * p3 * ... pn-1 * pn, where n > 0 and n is an integer
+     * 
+     * The method below takes a BigInteger as an argument and returns the largest
+     * prime factor of it.
+     */
+    public static long findFactors(BigInteger n) {
+        Long number = n.longValue();
+        Long largestPrimeDivisor = (long) 0;
+        for (Long index = (long) 0; index < number; index++) {
+            if (number % index == 0 && isPrimeDivisor(number) && index > largestPrimeDivisor) {
+                largestPrimeDivisor = index;
+            }
+        }
+
+        return largestPrimeDivisor;
+    }
+
+    public static boolean isPrimeDivisor(Long n) {
+        Long size = (long) 0;
+        for (Long index = n; index > 0; index--) {
+            if (n % index == 0) {
+                size++;
+            }
+        }
+
+        if (size == 2) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // Set-up
+        String NUM = "600851475143L";
+        final BigInteger N = new BigInteger(NUM);
+
+        System.out.println(findFactors(N));
     }
 }
