cca62b8ad817dc61c47b4cc64705f24126e78ef5
jybryce4 <jybryce4@gmail.com>
2019-04-05 19:35:51 -0500

    began problem 4 - palidrome number

---
+++ java/com/brycejamey/problem4/PalindromicNumber.java
@@ -0,0 +1,19 @@
+/*
+ * Project Euler Problem 4
+ * @ http://projecteuler.net/problem=4
+ * 
+ * This program finds the largest palindrome that is 
+ * the product of two 3-digit numbers
+ * 
+ * Solution by Jamey Bryce
+ */
+
+package com.brycejamey.problem4;
+
+public class PalindromicNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+}
