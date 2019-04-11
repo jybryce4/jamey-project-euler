ea34a58f3c270d8eaf908ab51bc315cfd6262259
Jamey Bryce <jamey@jameyubuntudesktop.home>
2019-04-11 15:20:36 -0500

    did problem 1 using java8 concepts

---
+++ java/com/brycejamey/problem1/java8_version/Multiples35.java
@@ -0,0 +1,14 @@
+import java.util.stream.IntStream;
+
+public class Multiples35 {
+
+	public static void main(String[] args) {
+		System.out.println(
+		    IntStream
+		        .range(0, 1000)
+		        .map(x -> x)
+		        .filter(x -> x % 3 == 0 || x % 5 == 0)
+		        .sum());
+	}
+
+}
